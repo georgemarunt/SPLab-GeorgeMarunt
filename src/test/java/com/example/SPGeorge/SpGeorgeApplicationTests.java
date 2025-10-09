38e7bb67bd00749b604e8e0285ca3f8d7146cccb
George Marunt <george.marunt04@e-uvt.ro>
2025-10-09 20:55:42 +0300

    Initial commit with README

---
+++ src/test/java/com/example/SPGeorge/SpGeorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SPGeorge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpGeorgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
